709429bfd813aecb1b5b91641cadecb8ac071849
箕作 天志 <69094508+T-Kisaku@users.noreply.github.com>
2022-04-19 20:38:44 +0900

    Supported

--- src/components/organizations/Container.svelte
+++ src/components/organizations/Container.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-    export let variant: 'all' | 'bottom' = 'all'
+    export let variant: 'all' | 'bottom' | 'article' | 'news' = 'all'
 </script>
 
 {#if variant === 'all'}
     <div class="py-20"><slot/></div>
+{:else if variant === 'article'}
+    <div class="pb-20 md:px-32 md:py-20">
+        <slot/>
+    </div>
+{:else if variant ==='news'}
+<div class="py-20 md:px-32">
+    <slot/>
+</div>
 {:else}
     <div class="pb-20"><slot/></div>
 {/if}
--- src/components/organizations/NewsLayout.svelte
+++ src/components/organizations/NewsLayout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
+	import Container from './Container.svelte';
 	import Paper from '@/src/components/atoms/Paper.svelte';
 	import Title from '../atoms/Title.svelte';
 
@@ -9,7 +10,8 @@
 
 <Title class="text-2xl mt-20">{title}</Title>
 <div class="text-center">公開日: {dayjs(publishedDate).format('YYYY年M月D日')}</div>
-
-<Paper class="mx-2 sm:mx-20 mt-20 mb-20 p-7">
-	<slot />
-</Paper>
+<Container variant="news">
+	<Paper class="p-7">
+		<slot />
+	</Paper>
+</Container>
